99d153dd12a989bd26d6d44b54df711cd5ba4991
changyunyeong <sally0109277@naver.com>
2024-02-03 20:53:13 +0900

    재귀

--- 00x0B/2447.py
+++ 00x0B/2447.py
@@ -1,10 +1,24 @@
 # 재귀는 n부터 생각할 것!
 # function define -> base condition -> recursion
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 
-def fun(n, i, j):
+def fun(n):
+    star = []
+    
     if n == 1:
-        return 
+        return ['*']
+    
+    ans = fun(n//3)
+    
+    for i in ans:
+        star.append(i*3)
+    for i in ans:
+        star.append(i + " "*(n//3) + i) # 빈칸 9,3,1
+    for i in ans:
+        star.append(i*3)
+    
+    return star
+
+print('\n'.join(fun(n)))
+
+# 배열에 별 하나씩 append 한다는 생각을 못 했음 (단순 반복문으로만 접근)
