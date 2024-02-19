30d267cea3caf5cdf853682b75130bcb8c59bd30
changyunyeong <sally0109277@naver.com>
2024-02-19 17:44:32 +0900

    백트래킹

--- 00x0C/6603.py
+++ 00x0C/6603.py
@@ -1,3 +1,24 @@
 import sys
 input = sys.stdin.readline
 
+def fun(depth, idx):
+    if depth == 6:
+        print(" ".join(map(str, lst)))
+        return
+    
+    for i in range(idx, k):
+        # if arr[i] not in lst:
+            lst.append(s[i])
+            fun(depth+1, i+1)
+            lst.pop()
+
+
+while True:
+    arr = list(map(int, input().split()))
+    k = arr[0]
+    s = arr[1:]
+    lst = []
+    fun(0,0)
+    if k == 0:
+        exit()
+    print()
