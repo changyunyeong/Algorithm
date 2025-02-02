a8bfa0380a89daf820b5a550f4bb79fa8d4d6a50
changyunyeong <sally0109277@naver.com>
2025-02-02 16:43:00 +0900

    ✨ feat: 단어 정렬

---
+++ BOJ/1181.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [input() for _ in range(n)]
+arr = list(set(arr))
+
+arr.sort()
+arr.sort(key = len)
+
+for i in arr:
+    print(i, end='')
