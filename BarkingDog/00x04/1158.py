75f78cd885b1bdf71f07fb900bd59f7f142cd50c
changyunyeong <sally0109277@naver.com>
2025-02-09 21:50:27 +0900

    ✨ feat: 요세푸스 문제

--- BarkingDog/00x04/1158.py
+++ BarkingDog/00x04/1158.py
@@ -1,11 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 n, k = map(int, input().split())
 arr = list(range(1, n+1))
 temp = []
 idx = 0
 
 for i in range(n):
-    idx += k-1
+    idx += k - 1
     idx %= len(arr) 
-    temp.append(str(arr.pop(idx)))
-    
-print("<",", ".join(temp)[:],">", sep='')
+    temp.append(arr.pop(idx))  
+
+print("<", ", ".join(map(str, temp)), ">", sep='')
